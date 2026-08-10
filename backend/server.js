c3647f8978ed6d4ec6de308ee799f33cc8f8dd04
Riahi Yasmine <riahiy52@gmail.com>
2026-08-10 03:48:37 +0100

    Clean project dependencies

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const taskRoutes = require("./routes/taskRoutes");
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
@@ -14,6 +15,7 @@ dotenv.config();
 const app = express();
 
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
